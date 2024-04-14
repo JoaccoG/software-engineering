02bf8058f75dceebeb87d14291fd0cb5464a0756
Joacco <joaquingodoy2407@gmail.com>
2024-04-14 21:47:11 +0200

    feat(stack): add tests for .size() method

--- src/data-structures/stack.test.ts
+++ src/data-structures/stack.test.ts
@@ -1,9 +1,9 @@
 import { Stack } from './stack';
 
 describe('Given a Stack data structure', () => {
-  let testStack: Stack<number[]>;
+  let testStack: Stack<number>;
   beforeEach(() => {
-    testStack = new Stack<number[]>();
+    testStack = new Stack<number>();
   });
 
   describe('When a new Stack is created', () => {
@@ -12,24 +12,38 @@ describe('Given a Stack data structure', () => {
     });
   });
 
-  describe('When we peek at an empty Stack', () => {
-    test('Then it should return undefined', () => {
+  describe('When we peek at an Stack', () => {
+    test('Then it should return the top item in the Stack or undefined if the Stack is empty', () => {
       expect(testStack.peek()).toBeUndefined();
+      testStack.push(1, 2, 3, 5, 8, 13, 20);
+      expect(testStack.peek()).toEqual(20);
+      testStack.push(40);
+      expect(testStack.peek()).toEqual(40);
     });
   });
 
   describe('When an item is pushed onto the Stack', () => {
     test('Then the item should be added to the Stack', () => {
-      testStack.push([1, 2, 3]);
-      expect(testStack.peek()).toEqual([1, 2, 3]);
+      testStack.push(1, 2, 3);
+      expect(testStack.push(4, 5, 6)).toBe(6);
     });
   });
 
   describe('When an item is popped from the Stack', () => {
     test('Then the item should be removed from the Stack', () => {
-      testStack.push([1, 2, 3]);
-      expect(testStack.pop()).toEqual([1, 2, 3]);
+      testStack.push(1, 2, 3);
+      expect(testStack.pop()).toEqual(3);
+      expect(testStack.pop()).toEqual(2);
+      expect(testStack.pop()).toEqual(1);
       expect(testStack.peek()).toBeUndefined();
+    });
+  });
+
+  describe('When we want to know the size of the Stack', () => {
+    test('Then it should return the number of items in the Stack', () => {
+      testStack.push(1, 2, 3);
+      testStack.push(4, 5, 6);
+      expect(testStack.size()).toBe(6);
     });
   });
 });
