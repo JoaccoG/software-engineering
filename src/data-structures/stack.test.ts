e099ee6756bc93a4737da9f8d3884b12e59ef1ce
Joacco <joaquingodoy2407@gmail.com>
2024-04-14 21:31:58 +0200

    feat(data-structures): add peek and push methods

--- src/data-structures/stack.test.ts
+++ src/data-structures/stack.test.ts
@@ -1,14 +1,27 @@
 import { Stack } from './stack';
 
 describe('Given a Stack data structure', () => {
-  let testStack: Stack;
+  let testStack: Stack<number[]>;
   beforeEach(() => {
-    testStack = new Stack();
+    testStack = new Stack<number[]>();
   });
 
   describe('When a new Stack is created', () => {
     test('Then it should be defined', () => {
       expect(testStack).toBeDefined();
+    });
+  });
+
+  describe('When we peek at an empty Stack', () => {
+    test('Then it should return undefined', () => {
+      expect(testStack.peek()).toBeUndefined();
+    });
+  });
+
+  describe('When an item is pushed onto the Stack', () => {
+    test('Then the item should be added to the Stack', () => {
+      testStack.push([1, 2, 3]);
+      expect(testStack.peek()).toEqual([1, 2, 3]);
     });
   });
 });
--- src/data-structures/stack.ts
+++ src/data-structures/stack.ts
@@ -1,3 +1,17 @@
-export class Stack {
-  constructor() {}
+export class Stack<T> {
+  #items: T[];
+
+  constructor() {
+    this.#items = [];
+  }
+
+  peek(): T | undefined {
+    return this.#items.length === 0
+      ? undefined
+      : this.#items[this.#items.length - 1];
+  }
+
+  push(item: T): void {
+    this.#items.push(item);
+  }
 }
